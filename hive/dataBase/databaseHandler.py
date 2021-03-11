1c56fdbdf6b26dfbb7605e0ceee97018589f4caa
CE-CF <70373078+CE-CF@users.noreply.github.com>
2021-03-11 13:54:38 +0100

    Database handler
    Jeg er smart

---
+++ hive/dataBase/databaseHandler.py
@@ -0,0 +1,14 @@
+from tableHandlers import *
+
+#All drone related commands
+
+#drone.insert('PythonTest', 'inserting', 12.345678, 12.345678)
+#drone.update('PythonTest', 'updating', 87.654321, 87.654321)
+#drone.delete('PythonTest')
+
+#All point of interest related commands
+
+#poi.new(00.000000, 00.000000)
+poi.new(11.111111, 11.111111)
+poi.new(22.222222, 22.222222)
+poi.new(33.333333, 33.333333)
