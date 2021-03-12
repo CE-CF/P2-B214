0e5d4b2f0582cfa5612d962f7dc83649c896d604
SimonJuhl99 <70756725+SimonJuhl99@users.noreply.github.com>
2021-03-12 14:37:35 +0100

    linear functions and local map generation

---
+++ hive/dmsRoutingModule/linearFunction.py
@@ -0,0 +1,51 @@
+import numpy as num
+
+''' This class can calculate the slope between two local coordinates
+    and find the intersection between this slope and the local y-axis '''
+
+
+class LinearFunction:
+    first_point = num.array([])
+    second_point = num.array([])
+    ''' These two arrays include the coordinates for 
+        the two points which linear function will be 
+        found using the methods in this class '''
+
+    # Constructor
+    def __init__(self, first_p, second_p):
+        self.first_point = first_p
+        self.second_point = second_p
+
+    ''' Calculates the difference between the two 
+        points' latitude and longitude coordinates '''
+    def calculate_difference(self):
+        return num.array([self.first_point[0] - self.second_point[0],
+                          self.first_point[1] - self.second_point[1]])
+
+    # This method calculates the slope between two points
+    def calculate_slope(self):
+        self.calculate_difference()
+        return (self.calculate_difference()[0] /
+                self.calculate_difference()[1])
+
+    # This method calculates the intersection between the slope and the local y-axis
+    def calculate_intersection(self):
+        return (self.first_point[0]
+                - (self.calculate_slope() * self.first_point[1]))
+
+    ''' The intersection formula:
+        b = y1 - a * x1 '''
+
+''' Vi skal lave nogle metoder til at finde arealet af firkanten, så vi 
+kan dele det op til n antal droner'''
+
+    def distance_in_meters(self):
+        pass
+
+    def calculate_area(self):
+        pass
+
+
+
+# input("Hvor mange droner vil du bruge?")
+#
--- hive/dmsRoutingModule/main.py
+++ hive/dmsRoutingModule/main.py
@@ -1,1 +1,35 @@
+import numpy as num
+from linearFunction import LinearFunction
+from math import sin, cos, sqrt, atan2, radians
 
+low_left = num.array([57.028481188091455, 9.948913866684594])
+low_right = num.array([57.028678655922064, 9.949934871541862])
+up_left = num.array([57.02879055029732, 9.948587540647853])
+up_right = num.array([57.02895776307729, 9.949560854500227])
+
+big_array = num.array([[57.028481188091455, 9.948913866684594],
+                      [57.028678655922064, 9.949934871541862],
+                      [57.02879055029732, 9.948587540647853],
+                      [57.02895776307729, 9.949560854500227]])
+
+
+
+# low_left = num.array([9.948913866684594, 57.028481188091455])
+# low_right = num.array([9.949934871541862, 57.028678655922064])
+# up_left = num.array([9.948587540647853, 57.02879055029732])
+# up_right = num.array([9.949560854500227, 57.02895776307729])
+
+local_low_left = num.subtract(low_left, low_left)
+local_low_right = num.subtract(low_right, low_left)
+local_up_left = num.subtract(up_left, low_left)
+local_up_right = num.subtract(up_right, low_left)
+
+# def LonLadToLocalSystem(lll, llr, lul, lur):
+
+
+# print(local_up_left, "    ", local_up_right, "\n        ", local_low_left, "             ", local_low_right)
+
+######################## los testos ##########################
+
+obj = LinearFunction(local_low_left, local_up_left)
+print(obj.calculate_intersection())
