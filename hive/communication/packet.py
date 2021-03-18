3b146bdc25451df8a62c7b5d28eb50a549e6788f
Simon Christensen <c3lphie@protonmail.com>
2021-03-18 11:11:49 +0100

    Only needs testing

--- hive/communication/packet.py
+++ hive/communication/packet.py
@@ -3,74 +3,6 @@ import ipaddress
 from ..exceptions.packet_exceptions import *
 
 
-def encode_packet(packet: Packet):
-    """Encode the packet object into bytes.
-
-    :param packet:
-    :type packet: Packet
-    :returns: bytes
-
-    """
-    if type(packet) is not Packet:
-        raise EncodeErrorPacket(packet, f"Argument is not of type: {type(Packet())} ")
-
-    packet_bytes_array = bytearray()
-    packet_bytes_array.append(packet.p_type)
-    packet_bytes_array += bytearray(packet.p_checksum)
-    packet_bytes_array += bytearray(packet.p_dest.packed)
-    packet_bytes_array += packet.p_data.encode()
-    packet_bytes_array += bytearray(0x00)
-    packet_bytes = bytes(packet_bytes_array)
-    return packet_bytes
-
-
-def decode_packet(packet_bytes: bytes):
-    """Decodes bytes and tries to convert them into a packet
-
-    :param packet_bytes:
-    :type packet_bytes:
-    :returns: Packet
-
-    """
-    packet_type = packet_bytes[0]
-    packet_checksum = packet_bytes[1:33]
-    packet_dest = packet_bytes[33:37]
-    packet_data = packet_bytes[37:]
-
-    if (
-        _calc_checksum(bytes(packet_type), bytes(packet_dest), bytes(packet_data))
-        != packet_checksum
-    ):
-        raise DecodeErrorChecksum(packet_bytes)
-    else:
-        packet = Packet(packet_type, packet_dest, packet_data.decode())
-        return packet
-
-
-def _calc_checksum(packet_type: bytes, packet_dest: bytes, packet_data: bytes):
-    """Calculates the checksum for a Packet
-
-    :param packet_type:
-    :type packet_type: bytes
-    :param packet_dest:
-    :type packet_dest: bytes
-    :param packet_data:
-    :type packet_data: bytes
-    :returns: bytes
-
-    """
-    checksum = hashlib.sha256()
-    type_bytes = packet_type
-    dest_bytes = packet_dest
-    data_bytes = packet_data
-
-    checksum.update(type_bytes)
-    checksum.update(dest_bytes)
-    checksum.update(data_bytes)
-
-    return checksum.digest()
-
-
 class Packet:
     """
     This is a class for the packets, in the Hive system, being sent between:
@@ -100,7 +32,7 @@ class Packet:
         self.p_type = p_type
         self.p_dest = p_dest
         self.p_data = p_data
-        self.p_checksum = _calc_checksum(
+        self.p_checksum = self.calc_checksum(
             bytes(self.p_type), self.p_dest.packed, self.p_data.encode()
         )
 
@@ -135,9 +67,9 @@ class Packet:
         return self._p_dest
 
     @p_dest.setter
-    def p_dest(self, p_dest):
+    def p_dest(self, dest):
         try:
-            self._p_dest = ipaddress.IPv4Address(p_dest)
+            self._p_dest = ipaddress.IPv4Address(dest)
         except:
             print(
                 f"""Something is wrong with destination address. Please make sure it is correct:
@@ -149,8 +81,8 @@ class Packet:
         return self._p_data
 
     @p_data.setter
-    def p_data(self, p_data):
-        self._p_data = p_data
+    def p_data(self, data: str):
+        self._p_data = data
 
     def dump(self):
         """Prints all of the content of the packet"""
@@ -162,3 +94,75 @@ class Packet:
         print(f"[DESTINATION]: {self.p_dest}")
         print(f"[DATA]: {self.p_data}")
         print("=====================================")
+
+    @staticmethod
+    def encode_packet(packet):
+        """Encode the packet object into bytes.
+
+        :param packet:
+        :type packet: Packet
+        :returns: bytes
+
+        """
+        if type(packet) is not Packet:
+            raise EncodeErrorPacket(
+                packet, f"Argument is not of type: {type(Packet())} "
+            )
+
+        packet_bytes_array = bytearray()
+        packet_bytes_array.append(packet.p_type)
+        packet_bytes_array += bytearray(packet.p_checksum)
+        packet_bytes_array += bytearray(packet.p_dest.packed)
+        packet_bytes_array += packet.p_data.encode()
+        packet_bytes_array += bytearray(0x00)
+        packet_bytes = bytes(packet_bytes_array)
+        return packet_bytes
+
+    @staticmethod
+    def decode_packet(packet_bytes: bytes):
+        """Decodes bytes and tries to convert them into a packet
+
+        :param packet_bytes:
+        :type packet_bytes:
+        :returns: Packet
+
+        """
+        packet_type = packet_bytes[0]
+        packet_checksum = packet_bytes[1:33]
+        packet_dest = packet_bytes[33:37]
+        packet_data = packet_bytes[37:]
+
+        if (
+            self.calc_checksum(
+                bytes(packet_type), bytes(packet_dest), bytes(packet_data)
+            )
+            != packet_checksum
+        ):
+            raise DecodeErrorChecksum(packet_bytes)
+        else:
+            packet = Packet(packet_type, packet_dest, packet_data.decode())
+            return packet
+
+    @staticmethod
+    def calc_checksum(packet_type: bytes, packet_dest: bytes, packet_data: bytes):
+        """Calculates the checksum for a Packet
+
+        :param packet_type:
+        :type packet_type: bytes
+        :param packet_dest:
+        :type packet_dest: bytes
+        :param packet_data:
+        :type packet_data: bytes
+        :returns: bytes
+
+        """
+        checksum = hashlib.sha256()
+        type_bytes = packet_type
+        dest_bytes = packet_dest
+        data_bytes = packet_data
+
+        checksum.update(type_bytes)
+        checksum.update(dest_bytes)
+        checksum.update(data_bytes)
+
+        return checksum.digest()
