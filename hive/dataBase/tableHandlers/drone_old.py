68fa44739472eead77e14d4c50405d317b3009b9
CE-CF <70373078+CE-CF@users.noreply.github.com>
2021-03-31 14:16:13 +0200

    Update and ready for code review
    All the _old files are functional

--- hive/dataBase/tableHandlers/drone_old.py
+++ hive/dataBase/tableHandlers/drone_old.py
@@ -1,35 +1,70 @@
 import mysql.connector
  
-#Insert new drone into table
 def insert(drone, state, latitude, longitude):
+    ''' Insert drone into hive.drone table with values stated 
+        
+        Parameters
+        ----------
+            drone: str
+                Name of the drone                       | 'dronename'
+            state: str
+                State of the drone                      | 'Offline' / 'Online'
+            latitude: float
+                The latitude coordinates of the drone   | 11.111111
+            longitude float
+                The longitude coordinates of the drone  | 11.111111
+    '''
+
     try:
-        connection = mysql.connector.connect(user='root', password='password', # Opretter forbindelse til MySQL databasen
+        #Establish connection to datase through mysql.connector
+        connection = mysql.connector.connect(user='root', password='password',
                                             host='localhost',
                                             database='hive')
         
-        cursor = connection.cursor()
+        cursor = connection.cursor() 
 
-        mySql_insert_query = """INSERT INTO drone (drone, state, latitude, longitude, time) 
+        mySql_insert_query = """INSERT INTO hive.drone (drone, state, latitude, longitude, time) 
                                 VALUES (%s, %s, %s, %s, CURRENT_TIME) """
-
+        
+        #Create tuple with values for the insert
         drone_data = (drone, state, latitude, longitude)
+        
+        #Execute query with values inserted
         cursor.execute(mySql_insert_query, drone_data)
+
+        #Commit previously executed query
         connection.commit()
         print("Record inserted successfully into drone table")
 
+    #Exception if there is a connection error, which diplays the error that occured
     except mysql.connector.Error as error:
         print("Failed to insert into MySQL table {}".format(error))
 
+    #Finally statement that closes connection to the database after the query is either committed or an error occurs
     finally:
         if connection.is_connected():
             cursor.close()
             connection.close()
             print("MySQL connection is closed")
 
-#Update drone already in table
 def update(drone, state, latitude, longitude):
+    ''' Update row where drone = drone with the values stated 
+        
+        Parameters
+        ----------
+            drone: str
+                Name of the drone                       | 'dronename'
+            state: str
+                State of the drone                      | 'Offline' / 'Online' 
+            latitude: float
+                The latitude coordinates of the drone   | 11.111111
+            longitude float
+                The longitude coordinates of the drone  | 11.111111
+    '''
+
     try:
-        connection = mysql.connector.connect(user='root', password='password', # Opretter forbindelse til MySQL databasen
+        #Establish connection to datase through mysql.connector
+        connection = mysql.connector.connect(user='root', password='password',
                                             host='localhost',
                                             database='hive')
         
@@ -42,22 +77,35 @@ def update(drone, state, latitude, longitude):
                                                     time = CURRENT_TIME 
                                                     WHERE drone = %s """
 
+        #Create tuple with values for the update
         drone_data = (state, latitude, longitude, drone)
+
+        #Execute query with updated values
         cursor.execute(mySql_update_query, drone_data)
+
+        #Commit previously executed query
         connection.commit()
         print("Record successfully updated drone table")
-
+    
+    #Exception if there is a connection error, which diplays the error that occured
     except mysql.connector.Error as error:
         print("Failed to update MySQL table {}".format(error))
 
+    #Finally statement that closes connection to the database after the query is either committed or an error occurs
     finally:
         if connection.is_connected():
             cursor.close()
             connection.close()
             print("MySQL connection is closed")
 
-#Delete specified drone from hive.drone
 def delete(drone):
+    ''' Delete row where drone = drone 
+        
+        Parameters
+        ----------
+            drone: str
+                Name of the drone | 'dronename'
+    '''
     try:
         connection = mysql.connector.connect(user='root', password='password', # Opretter forbindelse til MySQL databasen
                                             host='localhost',
@@ -67,14 +115,19 @@ def delete(drone):
 
         mySql_delete_query = """DELETE FROM hive.drone WHERE drone = %s"""
         
+        #Create tuple with drone that needs to be deleted
         drone_data = (drone,)
+
+        #Execute query to delete row
         cursor.execute(mySql_delete_query, drone_data)
         connection.commit()
         print("Record deleted successfully from drone table")
 
+    #Exception if there is a connection error, which display the error that occured
     except mysql.connector.Error as error:
         print("Failed to delete from MySQL table {}".format(error))
 
+    #Finally statement that closes connection to the database after the query is either committed or an error occurs
     finally:
         if connection.is_connected():
             cursor.close()
--- hive/dataBase/tableHandlers/route_old.py
+++ hive/dataBase/tableHandlers/route_old.py
@@ -1,19 +1,33 @@
 import mysql.connector
 
-#Function to insert string into middle of another string, used for checking file path
 def insert_string_lat(str, word):
+    '''Function to insert string into middle of another string, used for checking if columns exist'''
     return str[:3] + word
 
 def insert_string_long(str, word):
+    '''Function to insert string into middle of another string, used for checking if columns exist'''
     return str[:4] + word
 
 
-#Insert new poi into table
+#Insert new route into table
 def new(drone, type, *coordinates):
+    ''' Insert poi into hive.poi table with values stated 
+        
+        Parameters
+        ----------
+            drone: str
+                Name of the drone                       | 'dronename'
+            type: int
+                The route type                          | [1,2]
+            coordinates: list
+                The coordinates for the route           | 11.111111, 11.111111, 22.222222, 22.222222..... 99.999999, 99.999999
+    '''
     lat_list = coordinates[::2]
     long_list = coordinates[1::2]
+    #check if the type is within the allowed range
     if type in [1,2]:
         try:
+            #Establish connection to datase through mysql.connector
             connection = mysql.connector.connect(user='root', password='password', # Opretter forbindelse til MySQL databasen
                                                 host='localhost',
                                                 database='hive')
@@ -30,7 +44,7 @@ def new(drone, type, *coordinates):
             converted_i = ''
             exist = 0
             noexist = 0
-
+            #create query that checks how many rows exist compared to how big the insert is
             for i in range(len(lat_list)):
                 converted_i = '{}'.format(i+1)
                 longtitude = (insert_string_long('long', converted_i),)
@@ -43,13 +57,14 @@ def new(drone, type, *coordinates):
                 if row_count == 0:
                     noexist += 1
 
-            
+            #Creates new column names for longitude and latitude
             longitude = insert_string_long('long', converted_i)
             latitude = insert_string_lat('lat', converted_i)
             
             print("%i columns do exist"% exist)
             print("%i columns do not exist"% noexist)
             
+            #If no columns with coordinates exist, create the first columns
             if exist == 0:
                 mySql_route_new_query = """ALTER TABLE hive.route 
                 ADD COLUMN lat1 DECIMAL(10,6) AFTER type, 
@@ -59,6 +74,7 @@ def new(drone, type, *coordinates):
                 exist += 1
                 noexist -= 1
             
+            #If create columns that do not exist already
             for i in range(noexist):
                 newvalue = i+1+exist
                 lastvalue = i+exist
@@ -69,6 +85,7 @@ def new(drone, type, *coordinates):
                 cursor.execute(mySql_route_new_query, new_cor)
                 connection.commit()
             
+            #Check for type of route
             if type == 1:
                 type_converter = 'waypoint'
             else:
@@ -78,6 +95,8 @@ def new(drone, type, *coordinates):
             ending = (', received',)
             query_cor = [None]*len(coordinates)
             counter = 1
+
+            #Split up coordinates in latitude and longitude lists
             for i in range(len(coordinates)):
                 
                 converted_counter = '{}'.format(counter)
@@ -90,29 +109,37 @@ def new(drone, type, *coordinates):
                     #print("{0} is Odd".format(counter))
                     query_cor[i] = insert_string_long('long', converted_counter2)
 
+            #Create insert query information as strings
             query_placeholders = ', '.join(['%s'] * (len(coordinates)+2))
             query_columns = ', '.join(ds_list)
             query_cor = ', '.join(query_cor)
             query_end = ', '.join(ending)
 
+            #Combine insert query and information strings
             mySql_insert_route_query = ''' INSERT INTO hive.route (%s%s%s) VALUES (%s, CURRENT_TIME) ''' %(query_columns, query_cor, query_end, query_placeholders)
-
-            #print(mySql_insert_route_query)
+            
+            #Create tuples with values for the insert
             dronetype = (drone,type_converter)
+            
             route_data = dronetype+coordinates
-            #print("this is my route_data: %s"% list(route_data))
+            
+            #Execute query with values inserted
             cursor.execute(mySql_insert_route_query, route_data)
+            
+            #Commit previously executed query
             connection.commit()
 
 
-        
+        #Exception if there is a connection error, which display the error that occured
         except mysql.connector.Error as error:
             print("Failed to insert new route to table: {}".format(error))
         
+        #Finally statement that closes connection to the database after the query is either committed or an error occurs
         finally:
             if connection.is_connected():
                 cursor.close()
                 connection.close()
                 print("MySQL connection is closed")
+    #Error message if type is not within range
     else:
         print("Type not recognized, has to be in [1,2]")
