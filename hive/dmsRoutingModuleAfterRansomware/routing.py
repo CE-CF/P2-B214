e4e39c036a42a41f6b128a06f82d1e112b2971f7
SimonJuhl99 <70756725+SimonJuhl99@users.noreply.github.com>
2021-04-15 23:21:35 +0200

    point to function distance fixed

--- hive/dmsRoutingModuleAfterRansomware/routing.py
+++ hive/dmsRoutingModuleAfterRansomware/routing.py
@@ -1,7 +1,7 @@
 import numpy as np
 from coordinates import Coordinates
 from routingpackage.findfunctions import find_functions
-from routingpackage.findroutefunctions import run, getPlot
+import routingpackage.findroutefunctions
 from plots import Plot, show_plots
 
 
@@ -15,11 +15,8 @@ class Routing(Coordinates):
 
     def analyze_coordinates(self):
         self.functions = find_functions(self.local_c)
-        print(self.functions)
         self.plots.append(Plot(self.local_c, self.functions))
-        run(self.local_c, self.functions)
-        self.plots.append(getPlot())
+        origo_c = self.global_c[self.find_origo()]
+        routingpackage.findroutefunctions.run(self.global_c, self.local_c, self.functions, origo_c)
+        self.plots.append(routingpackage.findroutefunctions.getPlot())
         show_plots(self.plots)
-
-
-
