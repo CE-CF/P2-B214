0d029aab5352862686caa735a6fce2331f4a410e
CE-CF <70373078+CE-CF@users.noreply.github.com>
2021-05-10 15:44:35 +0200

    Fixed Relay Box

--- hive/relayBox/relayBoxUtilities/functions.py
+++ hive/relayBox/relayBoxUtilities/functions.py
@@ -7,10 +7,17 @@ import time
 import sys
 
 class Functions():
-    def __init__(self):
+    def __init__(self, droneID, dronePort, rbPort):
+        """ Creating a socket to the drone"""
+        self.droneID = droneID
+        self.drone = (droneID, dronePort)
+        self.rb = ('', rbPort)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(self.rb)
+        self.listenThread = self.listen()
         print("Function object has been created")
 
-    def ping(host):
+    def ping(self):
         """
         Returns True if host (str) responds to a ping request.
         Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
@@ -20,15 +27,14 @@ class Functions():
         param = '-n' if platform.system().lower()=='windows' else '-c'
 
         # Building the command. Ex: "ping -c 1 google.com"
-        command = ['ping', param, '5', host]
+        command = ['ping', param, '5', '{}'.format(self.droneID)]
 
         return subprocess.call(command) == 0
 
-    def send(message, delay):
+    def send(self, message, delay):
         """Try to send the message otherwise print the exception"""
         try:
-            sock1.sendto(message.encode(), tello1_address)
-            #sock2.sendto(message.encode(), tello2_address)
+            self.sock.sendto(message.encode(), self.drone)
             print("Sending message: " + message)
         except Exception as e:
             print("Error sending: " + str(e))
@@ -36,18 +42,29 @@ class Functions():
         # Delay for a user-defined period of time
         time.sleep(delay)
 
-    def receive():
+    def receive(self):
+        """Setting up the listener"""
         # Continuously loop and listen for incoming messages
         while True:
             # Try to receive the message otherwise print the exception
             try:
-                response1, ip_address = sock1.recvfrom(128) # The 128 is a buffer for the received message, it has to be large enough for any message
-                #response2, ip_address = sock2.recvfrom(128)
-                print("Received message: from Tello EDU #1: " + response1.decode(encoding='utf-8')) # UTF-8 stands for “Unicode Transformation Format - 8 bits.” It can translate any Unicode character to a matching unique binary string, and can also translate the binary string back to a Unicode character. 
-                print("Received message: from Tello EDU #2: " + response2.decode(encoding='utf-8'))
+                response1, ip_address = self.sock.recvfrom(1024) # The 1024 is a buffer for the received message, it has to be large enough for any message
+                print("Received message: from Tello EDU "+droneID+": "+response1.decode(encoding='utf-8')) # UTF-8 stands for “Unicode Transformation Format - 8 bits.” It can translate any Unicode character to a matching unique binary string, and can also translate the binary string back to a Unicode character. 
             except Exception as e:
                 # If there's an error close the socket and break out of the loop
                 sock1.close()
-                #sock2.close()
                 print("Error receiving: " + str(e))
                 break
+    
+    def listen(self):
+        """Creates udp response listener"""
+        receiveThread = threading.Thread(target=self.receive)
+        receiveThread.daemon = True
+        receiveThread.start()
+        return receiveThread
+
+    def closeConnection(self):
+        """ Closes socket and listener"""
+        self.listenThread.join()
+        self.sock.close()
+        print("Connection closed")
--- hive/relayBox/relayInitiation.py
+++ hive/relayBox/relayInitiation.py
@@ -7,91 +7,16 @@ import time
 import sys
 from relayBoxUtilities import *
 
-INTERVAL = 0.2
-# IP and port of Tello
-tello1_address = ('192.168.137.103', 8889)
-#tello2_address = ('192.168.0.66', 8889)
 
-# IP and port of local computer
-#local1_address = ('', 9010)
-local1_address = ('', 9011)
-
-# Create a UDP connection that we'll send the command to
-sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Bind to the local address and port
-sock1.bind(local1_address)
-#sock2.bind(local2_address)
-
-def send(message, delay):
-        """Try to send the message otherwise print the exception"""
-        try:
-            sock1.sendto(message.encode(), tello1_address)
-            #sock2.sendto(message.encode(), tello2_address)
-            print("Sending message: " + message)
-        except Exception as e:
-            print("Error sending: " + str(e))
-
-        # Delay for a user-defined period of time
-        time.sleep(delay)
-
-def receive():
-        # Continuously loop and listen for incoming messages
-        while True:
-            # Try to receive the message otherwise print the exception
-            try:
-                response1, ip_address = sock1.recvfrom(1024) # The 128 is a buffer for the received message, it has to be large enough for any message
-                #response2, ip_address = sock2.recvfrom(1024)
-                print("Received message: from Tello EDU #1: " + response1.decode(encoding='utf-8')) # UTF-8 stands for “Unicode Transformation Format - 8 bits.” It can translate any Unicode character to a matching unique binary string, and can also translate the binary string back to a Unicode character. 
-                print("Received message: from Tello EDU #2: " + response2.decode(encoding='utf-8'))
-                if response == 'ok':
-                    continue
-                out = response.replace(';', ';\n')
-                out = 'Tello State:\n' + out
-                report(out)
-                sleep(INTERVAL)
-            except Exception as e:
-                # If there's an error close the socket and break out of the loop
-                sock1.close()
-                #sock2.close()
-                print("Error receiving: " + str(e))
-                break
-
-# Create and start a listening thread that runs in the background
-# This utilizes our receive functions and will continuously monitor for incoming messages
-receiveThread = threading.Thread(target=receive)
-receiveThread.daemon = True
-receiveThread.start()
-
-send("command", 3)
-"""
-try:
-        index = 0
-        while True:
-            index += 1
-            response, ip = socket.recvfrom(1024)
-            if response == 'ok':
-                continue
-            out = response.replace(';', ';\n')
-            out = 'Tello State:\n' + out
-            report(out)
-            sleep(INTERVAL)
-    except KeyboardInterrupt:
-        curses.echo()
-        curses.nocbreak()
-        curses.endwin()
-"""
-#send("takeoff",8)
-#send("command", 3 )
-send('takeoff', 3)
-send('land', 1)
-# Close the socket
-sock1.close()
-#sock2.close()
-
-#testPing1 = functions.Functions.ping("192.168.137.103")
-#testPing1 = functions.Functions.ping("192.168.137.66")
-
-
-
+# IP and port of Tello and Relay Box
+tello1 = functions.Functions('192.168.137.103', 8889, 9011)
+if (tello1.ping() == True):
+    # Send connected drone to DMS
+    # Receive route navigation commands
+    tello1.send("command", 3)
+    tello1.send('takeoff', 3)
+    # Implement correction algorithm
+    tello1.send('land', 1)
+    tello1.closeConnection()
+else:
+    print("Cannot establish connection to tello1")
