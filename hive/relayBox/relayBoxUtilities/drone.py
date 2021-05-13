357d76c1ef4f3434cc9036f5053105a788a33d8b
CE-CF <70373078+CE-CF@users.noreply.github.com>
2021-05-13 09:17:11 +0200

    Fixed relay box

--- hive/relayBox/relayBoxUtilities/drone.py
+++ hive/relayBox/relayBoxUtilities/drone.py
@@ -4,18 +4,44 @@ import subprocess  # For executing a shell command
 import socket
 import threading
 import time
-import sys
 
-class Functions():
+class Drone():
     def __init__(self, droneID, dronePort, rbPort):
         """ Creating a socket to the drone"""
-        self.droneID = droneID
-        self.drone = (droneID, dronePort)
-        self.rb = ('', rbPort)
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind(self.rb)
-        self.listenThread = self.listen()
-        print("Function object has been created")
+        if (self.testSocket(droneID, dronePort, rbPort)) == True:
+            self.droneID = droneID
+            self.drone = (droneID, dronePort)
+            self.rb = ('', rbPort)
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.sock.bind(self.rb)
+            self.listenThread = self.listen()
+        else:
+            print("Drone object was not created.")
+
+    def testSocket(self, socket_ip, socket_port, bind_port):
+        """ Test to see if a socket is open """
+        a_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        
+        local = ('', bind_port)
+        location = (socket_ip, socket_port)
+        
+        a_socket.bind(local)
+
+        result_of_check = a_socket.connect_ex(location)
+
+        if result_of_check == 0:
+            print("Port is open")
+            output = True
+        else:
+            print("Port is not open")
+            output = False
+        a_socket.close()
+        #a_socket.shutdown(socket.SHUT_RDWR)
+
+        return output
+
+
+        
 
     def ping(self):
         """
@@ -32,7 +58,7 @@ class Functions():
         return subprocess.call(command) == 0
 
     def send(self, message, delay):
-        """Try to send the message otherwise print the exception"""
+        """Try to send CMD to drone, otherwise print exception"""
         try:
             self.sock.sendto(message.encode(), self.drone)
             print("Sending message: " + message)
@@ -49,10 +75,10 @@ class Functions():
             # Try to receive the message otherwise print the exception
             try:
                 response1, ip_address = self.sock.recvfrom(1024) # The 1024 is a buffer for the received message, it has to be large enough for any message
-                print("Received message: from Tello EDU "+droneID+": "+response1.decode(encoding='utf-8')) # UTF-8 stands for “Unicode Transformation Format - 8 bits.” It can translate any Unicode character to a matching unique binary string, and can also translate the binary string back to a Unicode character. 
+                print("Received message: from Tello EDU "+self.droneID+": "+response1.decode(encoding='utf-8')) # UTF-8 stands for “Unicode Transformation Format - 8 bits.” It can translate any Unicode character to a matching unique binary string, and can also translate the binary string back to a Unicode character. 
             except Exception as e:
                 # If there's an error close the socket and break out of the loop
-                sock1.close()
+                self.sock.close()
                 print("Error receiving: " + str(e))
                 break
     
@@ -67,4 +93,5 @@ class Functions():
         """ Closes socket and listener"""
         self.listenThread.join()
         self.sock.close()
+        self.sock.shutdown(socket.SHUT_RDWR)
         print("Connection closed")
